ab9275a715d10a07f41e0cdd8e33ba851ac413ec
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-14 17:41:41 -0700

    Functions for error handling

---
+++ error_functions.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * usage_err - prints error if no file or more than one arg
+ * was given
+ * Return: EXIT_FAILURE always
+ */
+int usage_err(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * open_file_error - prints error if file for any reason, can't be opened
+ * Return: EXIT_FAILURE always
+ */
+int open_file_error(char *filename)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * invalid_instruction - prints the line number an error ocurred on
+ * Return: EXIT_FAILURE always
+ */
+int invalid_instruction(int line_number, char *opcode)
+{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * malloc_error - prints error if can't malloc anymore
+ * Return: EXIT_FAILURE always
+ */
+int malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * line_num_error - prints error if the value after push is not an integer
+ * or there is no argument given
+ * Return: EXIT_FAILURE always
+ */
+int line_num_error(int line_number)
+{
+	fprtinf(stderr, "L%d: usage: push integer", line_number);
+	return (EXIT_FAILURE);
+}
