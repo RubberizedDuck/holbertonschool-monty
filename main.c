ee52365017000bd5d0a1bd1cd15748eb1a08e007
Luke Hackett <luke-hackett@outlook.com>
2022-08-15 18:30:16 -0700

    add while for check

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include "monty.h"
 
-int check_command(char *line, int line_count)
+int check_command(char *line, int line_num)
 {
 	char *command;
-	int n;
+	int n, idx;
+	stack_t *head = NULL;
 	instruction_t check[] = {
-		{"push", push},
+		{"push", _push},
 		{"pall", pall},
 		{NULL, NULL};
 	};
 	command = strtok(line, " ");
+
+	idx = 0;
+	while (check[idx].opcode != NULL)
+	{
+		if (*(check[idx].opcode) == command)
+		{
+			check[idx].f(&head, line_num);
+		}
+		idx++;
+	}
+
+	
 }
 
 int main(int argc, char *argv[])
 {
 	char *file, *stored, *line;
-	int filedes, file_read, line_count = 1;
+	int filedes, file_read, line_num = 1;
 
 	if (argc != 2)
 		usage_err();
@@ -39,8 +52,8 @@ int main(int argc, char *argv[])
 	line = strtok(stored, "\n");
 	while (line != NULL)
 	{
-		if (check_command(line) == 0)
-			line_count++;
+		if (check_command(line, line_num) == 0)
+			line_num++;
 		line = strtok(NULL, "\n");
 	}
 
