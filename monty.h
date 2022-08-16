7d4ad9a6fd8f0e98c1e823084f96273566990ca9
root <chaubnphan@gmail.com>
2022-08-15 17:35:18 -0700

    Create a nop function

--- monty.h
+++ monty.h
@@ -15,9 +15,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -30,8 +30,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* error functions */
@@ -40,5 +40,6 @@ int open_file_error(char *filename);
 int invalid_instruction(int line_number, char *opcode);
 int malloc_error(void);
 int line_num_error(int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
 
 #endif /* _MAIN_H_ */
