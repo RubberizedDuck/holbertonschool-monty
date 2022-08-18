768315e8d8a1df0439756fa18122fd433da41956
Luke Hackett <luke-hackett@outlook.com>
2022-08-18 15:53:44 -0700

    moved add func to new file

--- monty_funcs_2.c
+++ monty_funcs_2.c
@@ -44,7 +44,7 @@ void _nop(stack_t **stack, unsigned int line_number)
 
 /**
  * _sub - subtracts the first node from the second
- * @stack - pointer to start of stack
+ * @stack: pointer to start of stack
  * @line_number: count of lines executed
  * Return: void
  */
@@ -71,5 +71,38 @@ void _sub(stack_t **stack, unsigned int line_number)
 	total = (temp->next->n) - temp->n;
 	temp->next->n = total;
 
+	_pop(stack, line_number);
+}
+#include "monty.h"
+
+/**
+ * _add - adds the top two elements of the stack
+ * @stack: pointer to top of stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int total, count = 0;
+
+	temp = *stack;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	total = ((*stack)->n) + ((*stack)->next->n);
+	/* store total in second top element */
+	(*stack)->next->n = total;
+	/* remove top element */
 	_pop(stack, line_number);
 }
