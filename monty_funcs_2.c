b0aafce0024c4c08afc9cd18223ffcc6a0a14790
Luke Hackett <luke-hackett@outlook.com>
2022-08-18 15:46:02 -0700

    add sub func

--- monty_funcs_2.c
+++ monty_funcs_2.c
@@ -41,3 +41,35 @@ void _nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * _sub - subtracts the first node from the second
+ * @stack - pointer to start of stack
+ * @line_number: count of lines executed
+ * Return: void
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int total, count = 0;
+
+	temp = *stack;
+
+	while  (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	if (count < 2)
+	{
+		sub_error(line_number);
+	}
+
+	temp = *stack;
+	total = (temp->next->n) - temp->n;
+	temp->next->n = total;
+
+	_pop(stack, line_number);
+}
